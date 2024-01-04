2c07be46cf1bba71f1626fbbc60d5c2f9d390759
Paulo Lory <115539346+PauloLory-ionos@users.noreply.github.com>
2024-01-04 10:59:34 +0000

    ADD workflow + dockerfile image

--- connector/build.gradle.kts
+++ connector/build.gradle.kts
@@ -42,8 +42,8 @@ dependencies {
 
     implementation("${edcGroup}:data-plane-selector-client:${edcVersion}")
 
-    implementation(project(":edc-iono-nextcloud-extension:nextcloud-core"))
-
+    implementation(project(":edc-iono-nextcloud-extension:nextcloud-dataplane"))
+    implementation(project(":edc-iono-nextcloud-extension:nextcloud-provision"))
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
